7a915ae2cc7f8d1ea58d831d8c8d702209f0b77b
pysc <610312521@qq.com>
2018-08-29 13:09:49 +0800

    删除 音乐

--- Drama/models.py
+++ Drama/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class Drama(models.Model):
-    name = models.CharField('名称', max_length=100,null=True, blank=True)
-    img = models.CharField('图片链接地址', max_length=200, null=True,blank=True)
-    url = models.CharField('网址', max_length=200,primary_key=True)
-    intro = models.TextField('简介', max_length=2000,null=True, blank=True)
-    LOAD_TIME = models.DateTimeField('入库时间', blank=True,null=True,auto_now=True)
-
+    name = models.CharField('名称', max_length=100, null=True, blank=True)
+    img = models.CharField('图片链接地址', max_length=200, null=True, blank=True)
+    url = models.CharField('网址', max_length=200, primary_key=True)
+    intro = models.TextField('简介', max_length=2000, null=True, blank=True)
+    type = models.CharField('分类', max_length=10,null=True, blank=True)
+    LOAD_TIME = models.DateTimeField('入库时间', blank=True, null=True, auto_now=True)
--- Drama/teleplay.py
+++ Drama/teleplay.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from Drama.models import *
 import pymysql, time, datetime
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
 
 
 class Teleplay(object):
@@ -15,8 +17,7 @@ class Teleplay(object):
 
         urlpatterns = [
             url(r'^$', self.index, name='index'),
-            # url(r'^unhandle_alarm_show/$', self.unhandle_alarm_show, name='unhandle_alarm_show'),
-
+            # url(r'^details/$', self.details, name='details'),
         ]
         return urlpatterns
 
@@ -25,66 +26,20 @@ class Teleplay(object):
         return self.get_urls(), 'teleplay', self.name
 
     def index(self, request):
-        result = {}
-        total = Drama.objects.filter().all().count()
-        rows = Drama.objects.all().order_by('-LOAD_TIME')[1:10]
-        for rows in rows:
-            result['total'] = total
-            result['rows'].append({
-                'name': rows.name,
-                'img': rows.img,
-            })
-        return render(request, 'index.html', result)
 
-    # 未处理报警
-    def unhandle_alarm_show(self, request):
-        result = {
-            'total': None,
-            'rows': []
-        }
+        drama_list = Drama.objects.filter(type='电视剧').all()  # 获取所有contacts,假设在models.py中已定义了Contacts模型
+        paginator = Paginator(drama_list, 12)  # 每页12条
+        page = request.GET.get('page')
+        try:
+            rows = paginator.page(page)  # contacts为Page对象！
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            rows = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            rows = paginator.page(paginator.num_pages)
 
-        page_no = int(request.POST.get('page', 1))  # 第几页
-        page_size = int(request.POST.get('rows', 10))  # 每页显示几条数据
-        sort = request.POST.get('sort', '')  # 按照哪个字段进行排序
-        order = request.POST.get('order')  # 是升序还是降序排序
-        if order == 'desc':
-            sort = '-' + sort
-
-        BJSJ = request.POST.get('BJSJ', '')  #
-        SSPC = request.POST.get('SSPC', '')  #
-        BJJB = request.POST.get('BJJB', '')  #
-        BJLX = request.POST.get('BJLX', '')  #
-
-        if BJSJ:
-            year = BJSJ[0:4]
-            month = BJSJ[5:7]
-            day = BJSJ[8:10]
-
-        start = (page_no - 1) * page_size
-        end = page_no * page_size
-        print(start, end)
-        total = Teleplay.objects.filter(SFYCL=0).all().count()
-
-        searchCondition = {'BJSJ__date': BJSJ, 'SSPC': SSPC, 'BJJB': BJJB, 'BJLX': BJLX, }
-        kwargs = self.getKwargs(searchCondition)
-
-        if sort == '':
-            print('默认排序规则')
-            alarms = Teleplay.objects.filter(SFYCL=0).filter(**kwargs).order_by('-BJSJ')[start:end]
-        else:
-            print('用户自定义的排序规则', sort)
-            alarms = Teleplay.objects.filter(SFYCL=0).filter(**kwargs).order_by(sort)[start:end]
-
-        for teleplay in alarms:
-            result['total'] = total
-            result['rows'].append({
-                'BJSJ': teleplay.BJSJ.strftime('%Y-%m-%d %H:%M:%S'),
-                'SSPC': teleplay.SSPC,
-                'BJJB': teleplay.BJJB,
-                'BJLX': teleplay.BJLX,
-                'BJSM': teleplay.BJSM,
-            })
-        return JsonResponse(result, )
+        return render(request, 'index.html', {'rows': rows})
 
 
 teleplay = Teleplay()
